1c8b8772cef25f00f4a8457ec1ea438448e9872d
haraDATaku <phrboy@gmail.com>
2021-03-18 14:57:32 +0900

    first commit. All functions have been implemented.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace EasyTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start(&S)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(174, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sound(&F)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(255, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Exit(&X)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(143, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Min.";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(93, 9);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(49, 19);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 39);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "EasyTimer";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.Media;
+
+namespace EasyTimer {
+    public partial class Form1 : Form {
+        static String snd = @"C:\Windows\Media\Alarm01.wav";
+        static SoundPlayer wavePlayer;
+        const int defaultinterval = 25;
+
+        public Form1() {
+            InitializeComponent();
+            timer1.Enabled = false;
+            wavePlayer = new SoundPlayer();
+            setTitle();
+        }
+
+        private void toggle() {
+            bool en = timer1.Enabled;
+            if ( en ) {
+                button1.Text = "Start(&S)";
+            } else {
+                button1.Text = "Stop(&S)";
+                int msec = decimal.ToInt32( numericUpDown1.Value ) * 60 * 1000;
+                timer1.Interval = msec;
+                if ( !System.IO.File.Exists( snd ) ) {
+                    snd = @"C:\Windows\Media\tada.wav";
+                }
+                wavePlayer.SoundLocation = snd;
+                wavePlayer.LoadAsync();
+            }
+            timer1.Enabled ^= true;
+        }
+
+        private void button1_Click( object sender, EventArgs e ) {
+            toggle();
+        }
+
+        private void timer1_Tick( object sender, EventArgs e ) {
+            toggle();
+            wavePlayer.Play();
+        }
+
+        private void textBox1_TextChanged( object sender, EventArgs e ) {
+        }
+
+        private void button2_Click( object sender, EventArgs e ) {
+            OpenFileDialog fp = new OpenFileDialog();
+            fp.InitialDirectory = @"C:\Windows\Media";
+            fp.Filter = @"WAVファイル(*.wav)|*.wav";
+            if ( fp.ShowDialog() == DialogResult.OK ) {
+                if ( fp.FileName.Length > 0 ) {
+                    snd = fp.FileName;
+                    setTitle();
+                }
+            }
+        }
+
+        private void setTitle() {
+            String name = Path.GetFileName( snd );
+            this.Text = $"EasyTimer - {name}";
+        }
+
+        private void button3_Click( object sender, EventArgs e ) {
+            Application.Exit();
+        }
+    }
+}
